331da7cd56f1a96a0c3859766a46afbfa568904b
PriyanshCG <priyansh.patel.cg@gmail.com>
2025-09-15 11:50:11 +0530

    new c folder added and it consist of assignment

---
+++ C/assignment/q3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        printf("%c ", 'A' + i);
+    }
+    return 0;
+}
