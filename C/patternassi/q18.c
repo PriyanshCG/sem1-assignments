7f1a3953fb45a93d9487faaceb7a33ac4f892ee1
PriyanshCG <priyansh.patel.cg@gmail.com>
2025-09-23 11:22:51 +0530

    till 28

--- C/patternassi/q18.c
+++ C/patternassi/q18.c
@@ -1,16 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int n;
-    printf("Enter n: ");
     scanf("%d", &n);
-    for (int i = 0; i < n; i++)
+    for (int i = n; i >= 1; i--)
     {
         for (int j = 1; j <= i; j++)
         {
-            printf("%d ",j);
+            printf("%c ", j + 64);
         }
         printf("\n");
     }
-    return 0;
 }
--- C/patternassi/q9.c
+++ C/patternassi/q9.c
@@ -4,10 +4,10 @@ int main()
     int n;
     printf("Enter n: ");
     scanf("%d", &n);
-    for (int i = 0; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < i; j++)
-        {
+        for (int j = 0; j <= i; j++)
+        { 
             printf("%c ",j+65);
         }
         printf("\n");
