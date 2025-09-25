8e8255df1f913aa157e02449bc8de06edc6343a4
PriyanshCG <priyansh.patel.cg@gmail.com>
2025-09-25 12:47:09 +0530

    changes

--- C/patternassi/q31.c
+++ C/patternassi/q31.c
@@ -3,7 +3,6 @@ int main(){
     int n;
     printf("enter a number:");
     scanf("%d",&n);
-
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
             if(i==1||j==1||i==n||j==n){
@@ -15,7 +14,5 @@ int main(){
         }
         printf("\n");
     }
-
-
     return 0;
 }
